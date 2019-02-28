d33e7d24ecf1c66fb8e89b0a3adecc86f1517894
Sumanth V Rao <sumanthvrao@gmail.com>
2019-02-28 23:09:32 +0530

    Main application file and routes.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,23 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+var cors = require('cors');
+var bodyParser = require('body-parser');
+
+admin.initializeApp()
+
+// Express instance
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+// Setting up routes
+require('./routes/index')(app);
+
+// Run express as a firebase cloud function
+exports.app = functions.https.onRequest(app);
