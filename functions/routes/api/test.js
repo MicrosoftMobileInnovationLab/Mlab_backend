6f5cac1009d12fb19ca824d3fa52be746c23e1a2
Sumanth V Rao <sumanthvrao@gmail.com>
2019-05-22 06:46:55 +0530

    routes: addProject end point added.
    This endpoint uploads a project with its details
    (title, description, domain, PPT link);

--- functions/routes/api/test.js
+++ functions/routes/api/test.js
@@ -1,5 +1,7 @@
+var verifyToken = require('./../../middleware/verifyToken').verifyToken;
+
 module.exports = (app) => {
     app.get('/api/v1/helloWorld', (req, res)=>{
-       res.send("Hello World!");
+       res.send("Hello "+ req.usn +" from Firebase!");
     });
 };
