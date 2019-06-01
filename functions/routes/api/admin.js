d1469e6d8d18bfcb2be0dddeaab39f02d0e4a087
Sumanth V Rao <sumanthvrao@gmail.com>
2019-06-01 23:30:00 +0530

    fix: Regression caused by misspelling file names.

--- functions/routes/api/admin.js
+++ functions/routes/api/admin.js
@@ -1,5 +1,5 @@
-const utils = require('./../../utils/userutils')
-const firestoreUtils = require('./../../utils/fireStoreutils')
+const utils = require('./../../utils/userUtils')
+const firestoreUtils = require('./../../utils/fireStoreUtils')
 
 // TODO: Remove the following two dependencies and use firestoreUtils instead
 const firebase = require('firebase-admin')
